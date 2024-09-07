158c6f3a418eec82c83da2a9dc4b7e132dbf85bc
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-09-08 04:07:33 +0600

    just removed device_preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,14 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:get/get.dart';
 import 'package:lox/ui/home.dart';
 void main()
 {
-  runApp(
-    DevicePreview(
-      enabled: true,
-      builder: (context) => Main(),
-    ),
-  );
+  runApp(Main());
 }
 class Main extends StatelessWidget
 {
   @override
   Widget build(BuildContext context) {
-    return GetCupertinoApp(
-      useInheritedMediaQuery: true,
-      locale: DevicePreview.locale(context),
+    return CupertinoApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
     );
